80513596d4f316d66245ad18a1708d516da8479f
sigloiv <sigloiv@gmail.com>
2011-12-11 12:08:54 -0800

    using oop now

--- round_amazon.user.js
+++ round_amazon.user.js
@@ -2,22 +2,20 @@
 // @match http://*.amazon.com/*
 // ==/UserScript==
 
+
 var node = document
-var prices = node.getElementsByClassName("priceLarge")
-for (i = 0; i <= (prices.length - 1); i++)
+
+function roundPricesForClass(className)
 {
-tempPrice = prices[i].innerText.substr(1)
-tempPrice *= 2
-tempPrice = Math.round(tempPrice)
-tempPrice /= 2
-prices[i].innerText = "$" + tempPrice.toFixed(2)
+var prices = node.getElementsByClassName(className)
+for (i = 0; i <= (prices.length - 1); i++) {
+	tempPrice = prices[i].innerText.substr(1)
+	tempPrice *= 2
+	tempPrice = Math.round(tempPrice)
+	tempPrice /= 2
+	prices[i].innerText = "$" + tempPrice.toFixed(2)
+	}
 }
-var prices = node.getElementsByClassName("red t14")
-for (i = 0; i <= (prices.length - 1); i++)
-{
-tempPrice = prices[i].innerText.substr(1)
-tempPrice *= 2
-tempPrice = Math.round(tempPrice)
-tempPrice /= 2
-prices[i].innerText = "$" + tempPrice.toFixed(2)
-}
+
+roundPricesForClass("priceLarge")
+roundPricesForClass("red t14")
